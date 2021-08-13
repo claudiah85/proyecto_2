7be117dc6f8303330981af5d0fc0530e13577092
J. Xool-Tamayo <xool14@hotmail.com>
2021-08-12 22:38:51 -0700

    Es una estructura general  del script que no sé si funciona.
    Se baso del paso (Paso 4: Agrega botones) del Doctor Alejandro y la información de las tablas y las carpetas que ya se tienen. Les comparto esta información que podría ayudarnos http://shinyapps.org/apps/corona/    y   https://github.com/eparker12/nCoV_tracker

--- proyecto_2App/app.R
+++ proyecto_2App/app.R
@@ -1,39 +1,44 @@
 library(shiny)
+library(proyecto_2)
+library(dslabs)
+library(magrittr)
+library(ggplot2)
+library(tidyverse)
+library(ggridges)
+data(datos)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("coronaMEX"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  # Application title
+  titlePanel("coronaMEX"),
+  # Sidebar for parameters
+  sidebarLayout(
+    sidebarPanel(
+      ## Código para seleccionar paises
+      selectizeInput( 'states', label = h3("Seleccionar estados"), 
+                      choices = levels(proyecto_2$country), multiple=TRUE),
+      sliderInput( 'ylms', label = h3("Años contemplados"), min = 2020, 
+                   max = 2020, value = c(05-01-2020, 24-04-2020) ),
+      radioButtons( 'colorBy', label=h3("Colorear por"), 
+                    choices = c(estado="state", country="Nacional"), 
+                    selected = "Nacional" )
+    ),
+    mainPanel(
+      plotOutput("lifeExpPlot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  output$lifeExpPlot <- renderPlot({
+    proyecto_2 %>%
+      filter( state %in% input$states ) %>%
+      ggplot( aes( year, life_expectancy, col=get(input$colorBy), group=state )) +
+      geom_point( size=0.3 ) +
+      geom_line( ) +
+      coord_cartesian(xlim=c(input$ylms[1], input$ylms[2])) +
+      labs(col=input$colorBy)
+  })
 }
 
 # Run the application 
