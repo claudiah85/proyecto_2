6d0e6c7d9a9437c14c4b111d92f906097290bcea
GustavoSLB <san.luis.briseno.is.tecc@gmail.com>
2021-08-13 10:31:43 -0500

    Cambios_2

--- proyecto_2App/app.R
+++ proyecto_2App/app.R
@@ -4,7 +4,24 @@ library(magrittr)
 library(ggplot2)
 library(tidyverse)
 library(ggridges)
-data(corona_estados)
+library(here)
+library(readr)
+library(tidyr)
+
+datos <- read_csv(here("datos","corona_estados.csv"))
+#View(datos)
+#data(datos/corona_estados.csv)
+fecha_minima <- as.Date("05/01/2020", "%d/%m/%Y")
+fecha_maxima <- as.Date("24/04/2020", "%d/%m/%Y")
+#colnames(datos)
+#str(datos)
+
+
+datos_2 <- pivot_longer(datos, cols=4:114, names_to = "Fechas", values_to = "casos")
+
+datos_2$Fechas <- as.Date(datos_2$Fechas,"%d/%m/%Y")
+
+#view(datos_2)
 
 ui <- fluidPage(
   # Application title
@@ -13,13 +30,15 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       ## Código para seleccionar paises
-      selectizeInput( 'name', label = h3("Seleccionar estados"), 
-                      choices = levels(gapminder$name), multiple=TRUE),
-      sliderInput( 'ylms', label = h3("Mes"), min = 1, 
-                   max = 4, value = c(05-01-2020, 24-04-2020) ),
-      radioButtons( 'colorBy', label=h3("Colorear por"), 
-                    choices = c(estado="state", country="Nacional"), 
-                    selected = "Nacional" )
+      selectizeInput( 'nombre', label = h3("Seleccionar estados"), 
+                      choices = levels(as.factor(datos$nombre)), multiple=TRUE),##
+      sliderInput( 'ylms', label = h3("Dìas"), min = fecha_minima, 
+                   max = fecha_maxima, value = c(fecha_minima, fecha_maxima)),
+      
+      
+       # radioButtons( 'colorBy', label=h3("Colorear por"),
+       #               choices = c(estado="nombre"), 
+       #               selected = "nombre")
     ),
     mainPanel(
       plotOutput("lifeExpPlot")
@@ -27,12 +46,13 @@ ui <- fluidPage(
   )
 )
 
+
 # Define server logic
 server <- function(input, output) {
   output$lifeExpPlot <- renderPlot({
-    gapminder %>%
-      filter( state %in% input$states ) %>%
-      ggplot( aes( year, life_expectancy, col=get(input$colorBy), group=state )) +
+    datos_2 %>%
+      filter( nombre %in% input$nombre ) %>%
+      ggplot( aes( x= input$ylms ,y = input$casos, col=get(input$colorBy), group=nombre )) +
       geom_point( size=0.3 ) +
       geom_line( ) +
       coord_cartesian(xlim=c(input$ylms[1], input$ylms[2])) +
