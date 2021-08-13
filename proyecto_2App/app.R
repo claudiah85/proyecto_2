f8d710e9e1fe0075174cfc0af07374f8d3294145
xoolCCG <xool14@hotmail.com>
2021-08-12 17:44:37 -0700

    plantilla general

--- proyecto_2App/app.R
+++ proyecto_2App/app.R
@@ -4,7 +4,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("coronaMEX"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
